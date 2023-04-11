328872c8b647378bfb9ba81d942cf0266c2def75
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-04-11 15:35:51 +0300

    Create a service table and it's category enum

--- dbConfig/init.sql
+++ dbConfig/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE provider (
+CREATE TABLE IF NOT EXISTS provider (
   id SERIAL PRIMARY KEY
 , first_name VARCHAR(255) NOT NULL
 , last_name VARCHAR(255) NOT NULL
@@ -11,7 +11,7 @@ CREATE TABLE provider (
 , profile_image VARCHAR(255)
 );
 
-CREATE TABLE customer (
+CREATE TABLE IF NOT EXISTS customer (
   id SERIAL PRIMARY KEY
 , first_name VARCHAR(255) NOT NULL
 , last_name VARCHAR(255) NOT NULL
@@ -21,4 +21,29 @@ CREATE TABLE customer (
 , google_sub VARCHAR(255)
 , email_verified BOOLEAN DEFAULT false
 , email_token VARCHAR(255)
+);
+
+CREATE TYPE service_category_name AS ENUM (
+  'Cleaning',
+  'Babysitting',
+  'Pest Control',
+  'Plumbing',
+  'Electrical Repairs',
+  'Beauty',
+  'Miscellaneous'
+);
+
+CREATE TABLE IF NOT EXISTS service (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(70) NOT NULL,
+  description TEXT NOT NULL,
+  provider_id INTEGER NOT NULL,
+  price MONEY NOT NULL,
+  areas_covered VARCHAR(200)[],
+  availability VARCHAR(100)[],
+  category service_category_name NOT NULL,
+  is_available BOOLEAN DEFAULT false,
+  CONSTRAINT fk_provider
+    FOREIGN KEY (provider_id)
+      REFERENCES provider (id)
 );
