7cedadb258d2ad061102645d1b2040750a21eebd
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-04-13 13:30:15 +0300

    Use body in get services request rather than parameters

--- src/routes/service/services.js
+++ src/routes/service/services.js
@@ -6,29 +6,24 @@ const router = require("express").Router();
 // Get services of the current provider
 router.get("/services", authenticateToken, async (req, res, next) => {
   // const user = req.user;
-  const parameters = req.query;
-  console.log(parameters.provider);
+  const body = req.body;
 
   let sqlQuery =
-    'SELECT id, title, description, price, areas_covered AS "areasCovered", availability, category, is_available AS "isAvailable"' +
-    " FROM service";
+    'SELECT id, title, provider_id AS "providerID", description, price, areas_covered AS "areasCovered", availability, category, is_available AS "isAvailable"' +
+    " FROM service" + ' WHERE ';
 
-  if (parameters.provider) {
-    sqlQuery += ` WHERE service.provider_id = '${parameters.provider}'`;
-  } else {
-    return res
-      .status(400)
-      .json({ status: false, message: "Must provide provider parameter" });
+  if (body.provider) {
+    sqlQuery += ` service.provider_id = '${body.provider}'`;
   }
 
-  if (parameters.category) {
-    sqlQuery += ` AND category = '${parameters.category}'`;
+  if (body.category) {
+    sqlQuery += ` AND category = '${body.category}'`;
   }
 
   // how to check value is present in an array:
   // https://stackoverflow.com/questions/39643454/postgres-check-if-array-field-contains-value
-  if (parameters.area) {
-    sqlQuery += ` AND '${parameters.area}' = ANY(service.areas_covered)`;
+  if (body.area) {
+    sqlQuery += ` AND '${body.area}' = ANY(service.areas_covered)`;
   }
 
   try {
